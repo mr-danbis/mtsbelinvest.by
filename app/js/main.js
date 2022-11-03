9be4dba0f8367b1bfd70a263f72f66e185d15030
lis0jk <lissjkk@gmail.com>
2022-11-03 23:22:24 +0300

    Add rent block

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const mainPageSlider = new Slider();
   const mmPageSlider = new Slider();
+  const rentPageSlider = new Slider();
 
   if (wrapper.classList.contains("main-page")) {
     mainPageSlider.addSlider(".main-slider", {
@@ -28,6 +29,23 @@ document.addEventListener("DOMContentLoaded", function () {
     mmPageSlider.addSlider(".mm-slider", {
       items: 1,
       margin: 40,
+    });
+  }
+
+  if (wrapper.classList.contains("rent-page")) {
+    rentPageSlider.addSlider(".rent-slider__container", {
+      margin: 40,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        700: {
+          items: 2,
+        },
+        1430: {
+          items: 3,
+        },
+      },
     });
   }
 
@@ -50,6 +68,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
   openMobileSidebar();
+
+  class formTitle {
+    constructor(title, page) {
+      this.title = title;
+      this.page = page;
+    }
+    change(title, page) {
+      if (wrapper.classList.contains(page)) {
+        document.querySelector(".form__title").innerHTML = title;
+      }
+    }
+  }
+
+  const rentPageFormTitle = new formTitle();
+  rentPageFormTitle.change("Оформить заявку", "rent-page");
 });
 
 $(document).ready(function () {
@@ -66,31 +99,32 @@ $(document).ready(function () {
   });
 });
 
-
-
 function init() {
-	let map = new ymaps.Map('maps', {
-		center: [53.90279733757004,27.551026917561504],
-		zoom: 19
-	});
+  let map = new ymaps.Map("maps", {
+    center: [53.90279733757004, 27.551026917561504],
+    zoom: 19,
+  });
 
-	let placemark = new ymaps.Placemark([53.90279733757004,27.551026917561504], {}, {
-		iconLayout: 'defualt#image',
-		iconImageHref: 'https://cdn-icons-png.flaticon.com/512/2776/2776067.png',
-		iconImageSize: [38, 51],
-		iconImageOffset: [0, 0]
-	});
+  let placemark = new ymaps.Placemark(
+    [53.90279733757004, 27.551026917561504],
+    {},
+    {
+      iconLayout: "defualt#image",
+      iconImageHref: "https://cdn-icons-png.flaticon.com/512/2776/2776067.png",
+      iconImageSize: [38, 51],
+      iconImageOffset: [0, 0],
+    }
+  );
 
-
-	map.controls.remove('geolocationControl'); // удаляем геолокацию
-  map.controls.remove('searchControl'); // удаляем поиск
-  map.controls.remove('trafficControl'); // удаляем контроль трафика
-  map.controls.remove('typeSelector'); // удаляем тип
-  map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
-  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
-  map.controls.remove('rulerControl'); // удаляем контрол правил
-  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
-	map.geoObjects.add(placemark);
+  map.controls.remove("geolocationControl"); // удаляем геолокацию
+  map.controls.remove("searchControl"); // удаляем поиск
+  map.controls.remove("trafficControl"); // удаляем контроль трафика
+  map.controls.remove("typeSelector"); // удаляем тип
+  map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
+  map.controls.remove("zoomControl"); // удаляем контрол зуммирования
+  map.controls.remove("rulerControl"); // удаляем контрол правил
+  map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
+  map.geoObjects.add(placemark);
 }
 
 ymaps.ready(init);

34cd830ce8d22b82d5180ceba443c37ffa321a05
Daniel <dan.bisyukov@gmail.com>
2022-11-15 15:04:31 +0300

    Edited problems with maps zoom

--- app/js/main.js
+++ app/js/main.js
@@ -192,7 +192,7 @@ function init() {
     map.controls.remove("trafficControl"); // удаляем контроль трафика
     map.controls.remove("typeSelector"); // удаляем тип
     map.controls.remove("fullscreenControl"); // удаляем кнопку перехода в полноэкранный режим
-    map.controls.remove("zoomControl"); // удаляем контрол зуммирования
+    // map.controls.remove("zoomControl"); // удаляем контрол зуммирования
     map.controls.remove("rulerControl"); // удаляем контрол правил
     map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
     map.geoObjects.add(placemark);
